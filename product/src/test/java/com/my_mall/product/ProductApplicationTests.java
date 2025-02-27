8ed0e8ec40681c6ebd4e89fbb09f219c78d3ee1d
wxm <1466840216@qq.com>
2025-02-27 20:33:04 +0800

    initial

---
+++ product/src/test/java/com/my_mall/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my_mall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
